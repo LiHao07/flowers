d89e2a77bbfd0e01bd1189cd27280bb58e630190
Li Hao <gonens@163.com>
2019-04-21 17:34:47 +0800

    new

---
+++ test.py
@@ -0,0 +1,40 @@
+import sys
+sys.path.append('/Users/lihao/miniconda3/lib/python3.6/site-packages')
+import csv
+from torch.utils.data import DataLoader
+import torch
+from model import *
+from dataset import *
+
+use_cuda = torch.cuda.is_available()
+device = torch.device('cuda:{0}'.format(0) if use_cuda else 'cpu')
+
+model = FlowerModel().to(device)
+
+checkpoint = torch.load('checkpoint/epoch_49.pth')
+model.load_state_dict(checkpoint['model_state_dict'])
+
+test_dataset = TestDataset()
+test_loader = DataLoader(test_dataset, batch_size=100,
+                         shuffle=False, drop_last=False)
+model.eval()
+
+label_name = ['daisy', 'rose', 'tulip', 'dandelion', 'sunflower']
+with open('submit.csv','w') as File:
+    Writer=csv.writer(File)
+    Writer.writerow(['Id','Expected'])
+
+    with torch.no_grad():
+        for sample in test_loader:
+            data = sample['image'].to(device)
+            img_id = sample['id'].data.cpu().numpy()
+            print(img_id)
+            pred = model(data).data.cpu().numpy()
+            pred = pred.argmax(axis=1)
+            for i in range(data.shape[0]):
+                Writer.writerow([img_id[i], label_name[pred[i]]])
+
+
+
+
+
