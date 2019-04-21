c42bc17c4a8ec94c1fbeeefe6e7d8f1a6d127844
Li Hao <gonens@163.com>
2019-04-22 00:42:39 +0800

    new

--- MobileNet.py
+++ MobileNet.py
@@ -35,7 +35,8 @@ def conv_1x1_bn(inp, oup):
     return nn.Sequential(
         nn.Conv2d(inp, oup, 1, 1, 0, bias=False),
         nn.BatchNorm2d(oup),
-        nn.ReLU6(inplace=True)
+        nn.ReLU6(inplace=True),
+        nn.Dropout(0.5),
     )
 
 
