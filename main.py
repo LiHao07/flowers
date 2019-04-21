b80e9b0412dd69947d1401c55f871ab72160481e
Li Hao <gonens@163.com>
2019-04-21 22:42:52 +0800

    new

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ CUDA_VISIBLE_DEVICES = 0
 
 model = mobilenetv2(num_classes=5, input_size=224).to(device)
 weight = torch.load('pretrained/mobilenetv2-0c6065bc.pth')
-weight = {k: v for k, v in weight.items() if (k in model.state_dict()))}
+weight = {k: v for k, v in weight.items() if (k in model.state_dict())}
 model.load_state_dict(weight)
 
 optimizer = optim.Adam(model.parameters(), lr = args.lr)
