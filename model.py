35582adab46f5acd314e741f7ab31e1651b44a9b
Li Hao <gonens@163.com>
2019-04-21 18:20:52 +0800

    new

--- model.py
+++ model.py
@@ -16,13 +16,13 @@ class FlowerModel(nn.Module):
         self.conv2 = nn.Conv2d(16, 32, 3, padding=1, bias=True)
         self.bn2 = nn.BatchNorm2d(32)
 
-        #self.conv3 = nn.Conv2d(32, 64, 3, padding=1, bias=True)
-        #self.bn3 = nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d(32, 64, 3, padding=1, bias=True)
+        self.bn3 = nn.BatchNorm2d(64)
 
-        #self.conv4 = nn.Conv2d(64, 128, 3, padding=1, bias=True)
-        #self.bn4 = nn.BatchNorm2d(128)
+        self.conv4 = nn.Conv2d(64, 128, 3, padding=1, bias=True)
+        self.bn4 = nn.BatchNorm2d(128)
 
-        self.fc = nn.Linear(32, 5)
+        self.fc = nn.Linear(128, 5)
 
     def forward(self, x):
         x = F.relu(self.bn1(self.conv1(x)))
@@ -31,13 +31,13 @@ class FlowerModel(nn.Module):
         x = F.relu(self.bn2(self.conv2(x)))
         x = F.max_pool2d(x, 2, stride=2, padding=0)
 
-        #x = F.relu(self.bn3(self.conv3(x)))
-        #x = F.max_pool2d(x, 2, stride=2, padding=0)
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = F.max_pool2d(x, 2, stride=2, padding=0)
 
-        #x = F.relu(self.bn4(self.conv4(x)))
-        #x = F.max_pool2d(x, 2, stride=2, padding=0)
+        x = F.relu(self.bn4(self.conv4(x)))
+        x = F.max_pool2d(x, 2, stride=2, padding=0)
 
-        x = F.relu(x.mean(3).mean(2))
+        x = x.mean(3).mean(2)
 
         x = F.softmax(self.fc(x), dim=1)
         return x
